c807e6bd89b09f4685e6a1caca61e04e02279f52
Your Name <your.email@example.com>
2026-01-22 18:44:12 +0530

    Updated

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -10,7 +10,7 @@ import "./Infobox.css";
 
 export default function SearchBox({ updateInfo }) {
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   let getWeather = async (city) => {
     try {
