d22521932c940643d734df4e39d7f9c2c1154b4d
Your Name <your.email@example.com>
2026-01-22 18:47:26 +0530

    Updated

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,7 +6,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import DirectionsIcon from "@mui/icons-material/Directions";
 import { useState } from "react";
-import "./Infobox.css";
+import "./InfoBox.css";
 
 export default function SearchBox({ updateInfo }) {
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
